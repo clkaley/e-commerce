e96935a3a5a6c7c374434838a54099a5307eeda9
Aleyna Celik <clkkaley@gmail.com>
2022-09-20 17:37:23 +0300

    added categories on the Home page file

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,41 @@
-import React from 'react'
-
+import React from "react";
+import axios from "axios";
+import useSWR from "swr";
+import "./style.css";
+import random from "../../img/random.jpg"
+const fetcher = (url) => axios.get(url).then((res) => res.data);
 
 function Home() {
+  const { data, error } = useSWR(
+    "https://store.vrunibex.com/mobile2/mbProduct/CategoryList",
+    fetcher
+  );
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+  console.log("data", data.Result.TreeList);
   return (
     <div>
-   
+      <div className="container text-center mt-4">
+        <div className="row">
+          {data.Result.TreeList.map((item) => {
+            return (
+              item.ID < 11 && (
+                <div  key={item.ID} className="col-md-4 col-lg-3 col-sm-6 col-xs-12">
+                  <div>
+                    <div className="card">
+                      <img src={`${item.ImageUri==="" ? random : item.ImageUri}`}  alt="" />
+                      
+                    </div>
+                    <h6 className="title">{`${item.DisplayName.toUpperCase()}`}</h6>
+                  </div>
+                </div>
+              )
+            );
+          })}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
