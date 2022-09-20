79bff17f4f94c4463e2454d765faf3cb15899897
Aleyna Celik <clkkaley@gmail.com>
2022-09-20 14:37:02 +0300

    created route structure link and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
 
 function App() {
   return (
-    <div >
-    
-    </div>
+    <>
+    <Navbar/>
+    <Routes>
+       <Route path="/" element={<Home/>}/>
+       <Route path="products" element={<Products/>}/>
+    </Routes>
+    </>
   );
 }
 
