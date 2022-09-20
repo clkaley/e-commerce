e04a500c4509fca0ce88c41de1cba66d11f0b9eb
Aleyna Celik <clkkaley@gmail.com>
2022-09-20 17:22:16 +0300

    changed brand

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
        <nav className="navbar navbar-expand-lg bg-light">
         <div className="container">
           <Link className="navbar-brand" to="/">
-           E-commerce 
+           Vestel
           </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
