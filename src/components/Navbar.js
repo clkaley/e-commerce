2ac0c68bdd3df011f18a6b7150ed7e21347fae5b
Aleyna Celik <clkkaley@gmail.com>
2022-09-22 19:12:50 +0300

    changed navba structure

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,53 +8,91 @@ function Navbar() {
       <h1 className="brand-title m-2">
         VESTEL
       </h1>
-       <nav className="navbar navbar-expand-lg ">
-        <div className="container">
-          <Link className="navbar-brand" to="/">
-           Vestel
-          </Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <nav className="navbar navbar-dark navbar-expand-lg bg-light ">
+  <div className="container-fluid ">
+  <Link className="navbar-brand" to="/">
+   Vestel
+  </Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-
-              <li className="nav-item">
-                <Link
-                to='sign-in'
-                className="nav-link active" aria-current="page" href="#">
-                  Login
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                to='sign-up'
-                className="nav-link active" aria-current="page" href="#">
-                  Register
-              
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                to='products'
-                className="nav-link active" aria-current="page" href="#">
-                  Sepet
-                </Link>
-              </li>
-              {/* <li className="nav-item">
-                <Link
-                to='products'
-                className="nav-link active" aria-current="page" href="#">
-                  Login
-              
-                </Link>
-              </li> */}
-            </ul>
-          </div>
-        </div>
-      </nav>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+        <Link
+        to='sign-in'
+        className="nav-link active" aria-current="page" href="#">
+          Login
+        </Link>
+        </li>
+        <li className="nav-item">
+        <Link
+        to='sign-up'
+        className="nav-link active" aria-current="page" href="#">
+          Register
+      
+        </Link>
+        </li>
+        <li className="nav-item">
+        <Link
+        to='products'
+        className="nav-link active" aria-current="page" href="#">
+          Sepet
+        </Link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     </div>
   );
 }
 
 export default Navbar;
+
+
+{/* <nav classNameName="navbar navbar-expand-lg ">
+<div className="container">
+  <Link className="navbar-brand" to="/">
+   Vestel
+  </Link>
+  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<span className="navbar-toggler-icon"></span>
+</button>
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+      <li className="nav-item">
+        <Link
+        to='sign-in'
+        className="nav-link active" aria-current="page" href="#">
+          Login
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+        to='sign-up'
+        className="nav-link active" aria-current="page" href="#">
+          Register
+      
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+        to='products'
+        className="nav-link active" aria-current="page" href="#">
+          Sepet
+        </Link>
+      </li>
+      {/* <li className="nav-item">
+        <Link
+        to='products'
+        className="nav-link active" aria-current="page" href="#">
+          Login
+      
+        </Link>
+      </li> */}
+//     </ul>
+//   </div>
+// </div>
+// </nav> */}
