611f0de9ea1f28793e4640bf852eccac32a58483
Aleyna Celik <clkkaley@gmail.com>
2022-09-22 16:27:30 +0300

    added yup and controller for input text

--- src/pages/Authorization/SignUp/index.js
+++ src/pages/Authorization/SignUp/index.js
@@ -1,16 +1,31 @@
 import React from "react";
 import { useFormik } from 'formik';
-function SignUp() {
+import * as Yup from "yup";
 
+const SignupSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid email").required("Required"),
+    password: Yup.string()
+      .min(5, "Too Short!")
+      .max(15, "Too Long!")
+      .required("Required"),
+    passwordConfirm: Yup.string()
+      .oneOf([Yup.ref("password")], "Password does not match")
+      .required("Required"),
+  });
+
+function SignUp() {
     const formik = useFormik({
         initialValues: {
-          email: '',
-          password: '',
-          passwordConfirm: '',
+          email:'',
+          password:'',
+          passwordConfirm:'',
         },
+        validationSchema:SignupSchema,
         onSubmit:async(values,bag)=>{
             console.log(values);
-        }
+        },
+    
+       
         })
 
       
@@ -29,16 +44,11 @@ function SignUp() {
           <input
             type="email"
             className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
             name="email"
             onChange={formik.handleChange}
             value={formik.values.email}
             onBlur={formik.handleBlur}
           />
-          <div id="emailHelp" className="form-text">
-            We'll never share your email with anyone else.
-          </div>
         </div>
         <div className="mb-3">
           <label className="form-label">
