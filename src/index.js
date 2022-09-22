8f3fc691131edd005b767e41c48516d90b55cbe3
Aleyna Celik <clkkaley@gmail.com>
2022-09-22 18:21:52 +0300

    installed react-query and added structure into the index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-
+import { ReactQueryDevtools } from 'react-query/devtools'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
+const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
+   <QueryClientProvider client={queryClient}>
+   <ReactQueryDevtools initialIsOpen={false} />
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </QueryClientProvider>
   </>
 );
