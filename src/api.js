35c7a74208e5f4faef90f5173d5895f5ddab8925
Aleyna Celik <clkkaley@gmail.com>
2022-09-26 19:23:26 +0300

    added fetchLogin func

---
+++ src/api.js
@@ -0,0 +1,21 @@
+// import axios from "axios";
+
+// export const fetchRegister = async (input) => {
+// 	const { data } = await axios.post(
+// 		'https://www.vestel.com.tr/mobile2/mbUser/RegisterUser/?',
+// 		input
+// 	);
+// 	console.log("data", data);
+// 	return data;
+// };
+
+
+
+// export const fetchLogin = async (input) => {
+// 	const { data } = await axios.post(
+// 		"https://www.vestel.com.tr/mobile2/mbUser/Login?",
+// 		input
+// 	);
+// 	console.log("data", data);
+// 	return data;
+// };
