efcacb7e8620f4e906a1201b13f11d12c44590a4
Aleyna Celik <clkkaley@gmail.com>
2022-10-03 19:50:44 +0300

    changed login page title

--- src/pages/Authorization/SignIn/index.js
+++ src/pages/Authorization/SignIn/index.js
@@ -34,7 +34,7 @@ function SignIn() {
 // console.log(errors);
   return (
     <div>
-           <h1 className="title-reg text-center mt-5">Register</h1>
+           <h1 className="title-reg text-center mt-5">Login</h1>
       <form onSubmit={handleSubmit}>
         <div className="container">
           <div className="row">
